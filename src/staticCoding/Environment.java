ea0324969cd2f4a145e259fcefee1044b1e64b08
Jaime <alonsog.jaime@gmail.com>
2024-02-21 18:07:24 +0100

    organism life simulator submit version

---
+++ src/staticCoding/Environment.java
@@ -0,0 +1,169 @@
+package staticCoding;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+
+public class Environment {
+	
+
+
+	public static int numberGenerator(int intervalExclusive)
+	{
+		return ((int)(Math.random()*intervalExclusive));
+	}
+	
+	public static String nameGenerator() {
+		String generatedName = "";
+		
+		String[][] letter = {
+				{"a", "e", "i", "o", "u"},
+				{"p", "b", "r", "k", "w", "s", "z", "t", "y"}
+		};
+		
+		int randomIndex = ((numberGenerator(6)) + 2);
+		
+		for (int i = 0; i < randomIndex; i++) {
+			
+			int rowIndex = (numberGenerator(2));
+			
+			int columnIndex = (numberGenerator(letter[rowIndex].length));
+			
+			generatedName += letter[rowIndex][columnIndex];
+		}
+		
+		return generatedName;
+	}
+	
+
+	
+	public static void main(String[] args) {
+		
+		int organismInitiative = 0;
+		Scanner input = new Scanner(System.in);
+		
+		String state = "";
+		String begin = "era";
+		String end = "end";
+		int endOfTheWorld = 0;
+		
+		Organism.setDanger(numberGenerator(21));
+
+		ArrayList<Organism> organism = new ArrayList<Organism>();
+		
+		for (int i = 0; i < Organism.INITPOPULATION; i++) {
+		
+			organism.add(new Organism(numberGenerator(4), nameGenerator()));
+		}
+		
+		for (int i = 0; i < organism.size(); i++) {
+			organism.get(i).getOrganism();
+		}
+		
+		Organism.getEcosystem();
+		
+		do {
+			System.out.println('\n' + "Write 'era' to begin");
+			state = input.next();
+			if(state.equals(begin))
+			{
+				try 
+				{
+					do 
+					{
+						//all the organisms can do an action or none					
+						for (int i = 0; i < organism.size(); i++) 
+						{
+							
+							organismInitiative = numberGenerator(5);
+							
+							//checks if organism is alive
+							if(organism.get(i).isAlive())
+							{
+									switch(organismInitiative) 
+									{
+									case 0: 
+										//does nothing
+										System.out.println('\n' + organism.get(i).getName() + " sleeps");
+
+										break;
+									case 1:
+										organism.get(i).reproduce();
+										System.out.println('\n' + organism.get(i).getName() + " has reproduce");
+			
+										break;
+									case 2:
+										organism.get(i).hunt();	
+										System.out.println('\n' + organism.get(i).getName() + " has hunt");
+			
+										break;
+									case 3:
+										organism.get(i).scape();	
+										System.out.println('\n' + organism.get(i).getName() + " has scaped");
+			
+										break;
+									case 4: 
+										organism.get(i).eat();	
+										System.out.println('\n' + organism.get(i).getName() + " has eaten");
+			
+										break;
+									}
+									organism.get(i).getOld();
+									organism.get(i).vitalCheck();
+									organism.get(i).getVitals();
+									if(!organism.get(i).isAlive()) System.out.println('\n' + organism.get(i).getName() + " has died");
+									System.out.print("--------------------");
+									
+							}
+							TimeUnit.SECONDS.sleep(1);
+							
+						}
+						
+						endOfTheWorld += 2;
+						
+					} 
+					while(endOfTheWorld != 40);
+					
+					System.out.print('\n' + "-*-*-*-*-*-*-*-*-*-*-*-*");
+					Organism.getEcosystem();
+					System.out.print("-*-*-*-*-*-*-*-*-*-*-*-*" + '\n');
+				
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+				
+			}
+			else 
+			{
+				System.out.println("Write 'era' to begin or 'end' to exit");
+				state = input.next();
+			}
+		}
+		while(state.equals(end));
+		
+	input.close();
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/staticCoding/Organism.java
@@ -0,0 +1,184 @@
+package staticCoding;
+
+public class Organism {
+	
+	final private double AUREO = 1.618034;
+	
+	final public static int INITPOPULATION = 8;
+	private static String [] ecosystem = {"warm", "cold", "humid", "water"};
+	private static int organismsCounter;
+	private static int organismsAlive;
+	private static int organismsDead;
+	private static int danger;
+	
+	private String organismEcosystem;
+	//private int ecosystemIndex;
+	private String name;
+	private int longevity;
+	private int health;
+	private boolean isAlive;
+	private boolean isLonesome;
+	private boolean isHermaphrodite;
+	
+	//Constructor
+	public Organism(int ecosystemIndex, String name)
+	{
+		int loneliness = (int)(Math.random()*11);
+		int sexuality = (int)(Math.random()*11);
+		
+		//this.ecosystemIndex = ecosystemIndex;
+		organismEcosystem = ecosystem[ecosystemIndex];
+		
+		this.name = name;
+		
+		this.health = (int)(Math.random()*100);
+		
+		this.longevity = (int)(this.health*AUREO);
+		
+		if(longevity < 5) 
+		{
+			this.setAlive(false);
+			organismsDead++;
+		}
+		else 
+		{
+			this.setAlive(true);
+		}
+		
+		if(loneliness > 5)
+		{
+			this.isLonesome = false;
+		}
+		else this.isLonesome = true;
+		
+		if(sexuality > 5)
+		{
+			this.isHermaphrodite = false;
+		}
+		else this.isHermaphrodite = true;
+		
+		organismsCounter++;
+		
+	}
+	
+	//Getters & Setters
+	public static int getOrganismsCounter()
+	{
+		return organismsCounter;
+	}
+	
+	public static int getOrganismsAlive()
+	{
+		updateOrganismsAlive();
+		return organismsCounter;
+	}
+	
+	public boolean isAlive() {
+		return isAlive;
+	}
+	
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	
+	public static int getDanger() {
+		return danger;
+	}
+	
+	public static void setDanger(int danger) {
+		Organism.danger = danger;
+	}
+	
+	public static void organismDies() 
+	{
+		organismsDead++;
+	}
+	
+	public static void updateOrganismsAlive() 
+	{
+		organismsAlive = (organismsCounter - organismsDead);
+	}
+	
+	public static void getEcosystem()
+	{
+		updateOrganismsAlive();
+		System.out.println('\n' + "danger: " + danger + '\n' + "organisms counter: " + organismsCounter + '\n' + "organisms alive: " + organismsAlive + '\n' + "organisms dead: " + organismsDead);
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void getOrganism()
+	{
+		System.out.println('\n' + "name: " + name + '\n' + "alive? "+ isAlive + '\n' + "ecosystem: " + organismEcosystem + '\n' + "longevity: " + longevity + '\n' + "health: " + health + '\n' + "lonely? " + isLonesome + '\n' + "Hermaphrodite? " + isHermaphrodite);
+	}
+
+	public void getVitals()
+	{
+		System.out.println('\n' + name + " alive? " + "[" + isAlive + "]" + '\n' + "health: " + "{" + health + "}" + '\n' + "longevity: " + "(" + longevity + ")");
+	}
+	
+	
+	//Object methods
+	public void eat()
+	{
+		health = health + 20;
+	}
+	
+	public void reproduce()
+	{
+		if(this.isHermaphrodite)
+		{
+			health = health + 50;
+			//new Organism(this.ecosystemIndex ,this.name + " child");
+			//organismsCounter++;
+			//this is wrong, the correct way would be to return a new object and to index that into the ArrayList. For the purpose of this exercise I'll leave it like this.
+
+		}
+		else longevity = longevity + 20;
+	}
+	
+	public void hunt()
+	{
+		health = health + 50;
+	}
+	
+	public void die()
+	{
+		isAlive = false;
+		organismsDead++;
+	}
+	
+	public void getOld()
+	{
+		if(health != 0)
+			health = (health  - (int)(1.5 * danger));
+		else longevity = (longevity - 20);
+	}
+	
+	public void vitalCheck()
+	{
+		if(longevity <= 0)
+			die();
+		if(health >= 100) longevity = longevity + 10;
+		if(health <= 0) longevity = longevity - 50;
+	}
+	
+	public void scape()
+	{
+		if(!isLonesome)
+			longevity = longevity - 20;
+		else longevity = longevity - 50;
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
